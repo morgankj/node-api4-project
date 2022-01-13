e9e23f1480b4a000e209a9cbb322741fbb2d1fe3
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-13 16:12:09 -0600

    Progess on index.js - just placeholder data hard coded in for responses

--- index.js
+++ index.js
@@ -5,11 +5,17 @@ app.use(express.json());
 
 const port = process.env.PORT;
 
-app.get('/users', (req, res) => {});
+app.get('/users', (req, res) => {
+    res.status(200).json({ message: 'Here is where you would get a list of users' });
+});
 
-app.post('/register', (req, res) => {});
+app.post('/register', (req, res) => {
+    res.status(201).json({ message: 'Here is your response after registering an account' });
+});
 
-app.post('/login', (req, res) => {});
+app.post('/login', (req, res) => {
+    res.status(200).json({ message: 'You have logged in' });
+});
 
 
 
