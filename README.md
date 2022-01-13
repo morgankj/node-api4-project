9997592b9b3538e189632f6e1fcd2a225ada7d16
Morgan Jones <morganjones@Morgans-Air.attlocal.net>
2022-01-13 16:54:44 -0600

    Can pass user info into fake-db to be saved

--- README.md
+++ README.md
@@ -10,7 +10,7 @@ Your assignment page on Canvas should contain instructions for submitting this p
 
 ### Task 2: Minimum Viable Product
 
-- [ ] Build a simple API:
+- [X] Build a simple API:
 
   - Create a `.gitignore` file executing `npx gitignore node`
   - Create a `package.json` file executing `npm init --y`
@@ -26,7 +26,7 @@ Your assignment page on Canvas should contain instructions for submitting this p
     | POST   | /api/register | Creates a user from { username, password } in the `request body`, responds with newly created user. |
     | POST   | /api/login    | Checks { username, password } in the `request body`, responds with a welcome message. |
 
-- [ ] Deploy your app to [Heroku](https://heroku.com). The link to the deployed API is your submission deliverable
+- [X] Deploy your app to [Heroku](https://heroku.com). The link to the deployed API is your submission deliverable
 
 ### Important Notes
 
--- index.js
+++ index.js
@@ -3,18 +3,35 @@ const express = require('express');
 const app = express();
 app.use(express.json());
 
-const port = process.env.PORT;
+const port = process.env.PORT || 9000;
+
+const users = [
+    { 
+        id: 1,
+        name: 'Jamie'
+    },
+    {
+        id: 2,
+        name: 'Claire'
+    },
+    {
+        id: 3,
+        name: 'Brianna'
+    }
+];
 
 app.get('/', (req, res) => {
     res.send('<h1>WELCOME, FOOLS</h1>');
 });
 
 app.get('/users', (req, res) => {
-    res.status(200).json({ message: 'Here is where you would get a list of users' });
+    res.status(200).json(users);
 });
 
 app.post('/register', (req, res) => {
-    res.status(201).json({ message: 'Here is your response after registering an account' });
+    const newUser = { id: Date.now(), name: req.body.name, password: req.body.password };
+    users.push(newUser);
+    res.status(201).json(newUser);
 });
 
 app.post('/login', (req, res) => {
